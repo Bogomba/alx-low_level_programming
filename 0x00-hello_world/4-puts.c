38d4cba548de8824caf595933560a8f8662d0bca
Bogomba <bridgetbogomba@gmail.com>
2022-06-02 08:38:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point, prints programming is like
- *
  * Return: Always 0 (Success)
- * /
- * int main(void)
+ */
+int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+} 
