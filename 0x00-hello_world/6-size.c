5ecc9601192dc93f9fd5babf2e251dd66c1505b8
Bogomba <bridgetbogomba@gmail.com>
2022-06-02 12:48:49 -0700

    compiler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -Entry point, prints the size of various types on the computer
+ * Return: Always 0 (Success)
+ */
+struct size
+int size  ("\"-m", "\"-m6")
+{
+	puts("Size of a char: 1 byte(s");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)"); 
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return(0);
+}
