72f1c2c5fd3079741236138ab3c53369fbf1e4f5
Bogomba <bridgetbogomba@gmail.com>
2022-06-05 06:20:11 -0700

    lowercase in reverse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry poin , print 0 to 9 without variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int n;
+
+	if (n < 0)
+	{
+	putchar('-');
+	n = -n;
+	}
+
+	; n <= 10; n++)
+	putchar("%d, n");
+	putchar("\n");
+	return (0);
