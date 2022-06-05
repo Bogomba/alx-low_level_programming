1828b707641ac99d89ab98bf9fdb381f77764827
Bogomba <bridgetbogomba@gmail.com>
2022-06-05 01:50:10 -0700

    without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point, prints alphabet without q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char lower;
+	for (lower = 'a'; lower <= 'z'; lower++)
+	if (lower != 'q' && lower != 'e')
+	putchar(lower);
+	putchar('\n');
+	return (0);
+}
