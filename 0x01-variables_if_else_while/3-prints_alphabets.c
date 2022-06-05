9520bf78098ca0cb749a8b52144495f4583a39e3
Bogomba <bridgetbogomba@gmail.com>
2022-06-05 00:37:03 -0700

    lowerupper

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>                                                                                                                      
+/**                                                                            
+ * main -Entry point, prints alphabet in lower case                                                                                   
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char lower;
+		char upper;
+	for (lower = 'a'; lower <='z'; lower++)
+		for (upper = 'A'; upper <='Z'; upper++)                                 putchar(lower);
+	putchar(upper);
+	putchar('\n');
+	return (0);
+}
