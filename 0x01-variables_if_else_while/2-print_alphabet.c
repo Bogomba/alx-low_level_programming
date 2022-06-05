3a9ef13b1c3bfb73f64048850048d8a85583e740
Bogomba <bridgetbogomba@gmail.com>
2022-06-04 23:50:58 -0700

    spacing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
  * main -Entry point, prints alphabet in lower case
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
 	char alphabet;
 
-	for (alphabet='a'; alphabet<='z'; alphabet++)
+	for (alphabet = 'a'; alphabet < = 'z'; alphabet++)
 	putchar(alphabet);
 	putchar('\n');
-	return(0);
+	return (0);
 }
 
