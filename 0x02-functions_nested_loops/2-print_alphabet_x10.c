d03ed5dbabb2bd772df1fa878fdce9deecb3dcba
Bogomba <bridgetbogomba@gmail.com>
2022-06-07 15:55:18 -0700

    spacing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,12 @@ void print_alphabet_x10(void)
 	int i;
 	{
 	for (i = 0; i < 10; i++)
+
 		for (lower = 'a'; lower <= 'z'; lower++)
 		{
 			_putchar(lower);
 		}
 			_putchar('\n');
 	}
+	_putchar('\n');
 }
