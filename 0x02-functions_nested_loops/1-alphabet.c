4291fafbaa302e0c29cc31de8070cacd96b68654
Bogomba <bridgetbogomba@gmail.com>
2022-06-07 12:14:46 -0700

    a a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char lower;
+	
+	for (lower = 'a', lower <= 'z', lower++)	
+	print_alphabet("%c"; lower);
+	print_alphabet('\n');
+	return (0);
+	
+}
