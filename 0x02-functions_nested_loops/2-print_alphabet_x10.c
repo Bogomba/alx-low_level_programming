4bcfba42b24c503ff53fe17ad89c5bca0ed6125b
Bogomba <bridgetbogomba@gmail.com>
2022-06-07 15:50:33 -0700

    indent

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,11 @@ void print_alphabet_x10(void)
 	char lower;
 	int i;
 	{
-	for (lower = 'a'; lower <= 'z'; lower++)
-	_putchar(lower );
-	_putchar('\n');
-	}
 	for (i = 0; i < 10; i++)
-
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+			_putchar('\n');
+	}
 }
