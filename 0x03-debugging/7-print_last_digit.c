d96075f8d4418655cf9b02f155dd693aaece94d6
Bogomba <bridgetbogomba@gmail.com>
2022-06-08 11:28:26 -0700

    a function that prints the last digit of a number

---
+++ 0x03-debugging/7-print_last_digit.c
@@ -0,0 +1,14 @@
+include"main.h"
+/**
+ * print_last_digit - prints last digit of int
+ * @x: - character
+ * Return: Values of last digit
+ */
+int print_last_digit(int x)
+{
+	int x;
+
+	if (x > 0; x++)
+	_putchar(x, x % 10);
+	return (value of last digit);
+}
