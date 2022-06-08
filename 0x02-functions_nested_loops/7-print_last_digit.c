81b13345679abd4907e54c98127f46c210d0e483
Bogomba <bridgetbogomba@gmail.com>
2022-06-08 13:50:29 -0700

    spacing

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,24 @@
 #include"main.h"
-#include<stdio.h>
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: -character
  * Return: Always 0
  */
-int print_last_digit(int n % 10)
+int print_last_digit(int n)
 {
-	int n;
-
-	for (n < 0; n = 0; n > 0)
+	if (n < 0)
 	{
-	_putchar("n, n%10", n);
-	return (0);
+		n = -(n % 10);
 	}
+	else if (n > 0)
+	{
+		n = n % 10;
+	}
+	else
+	{
+		n = 0;
+	}
+	_putchar(n + '0');
+	return (n);
 }
