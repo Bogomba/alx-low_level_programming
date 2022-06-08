daaede81fe492f70052f32cb0def075cfdfddfca
Bogomba <bridgetbogomba@gmail.com>
2022-06-07 22:40:54 -0700

    spacing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,10 +10,10 @@ int print_sign(int n)
 	if (n > 0)
 	_putchar('+');
 	return (1);
-	if else(n = 0)
+	else if (n == 0)
 	_putchar('0');
 	return (0);
 	else
-	_puthcar('-');
+	_putchar('-');
 	return (-1);
 }
