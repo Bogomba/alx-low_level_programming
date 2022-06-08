28c846b86233ccd782906e516af7eb36c711365e
Bogomba <bridgetbogomba@gmail.com>
2022-06-08 13:25:52 -0700

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry poin , print 0 to 9 without variables
+ * main - Entry point , print 0 to 9 without variables
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -8,13 +8,8 @@ int main(void)
 {
 	int n;
 
-	if (n < 0)
-	{
-	putchar('-');
-	n = -n;
-	}
-
-	; n <= 10; n++)
-	putchar("%d, n");
-	putchar("\n");
+	if (n = 0; n <= 9; n++)
+	putchar("%d"; n);
+	putchar('\n');
 	return (0);
+}
