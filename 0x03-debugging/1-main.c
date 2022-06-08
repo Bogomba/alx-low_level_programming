aaac6e205b3d9dc3b5b9327383a9500a6a73e2af
Bogomba <bridgetbogomba@gmail.com>
2022-06-08 09:56:23 -0700

    spacing

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	while (i < 10) /*causes infinite loop*/
+
+	while (i < 10) /*causes output to go to an infinite loop*/
 	{
 		putchar(i);
 	}
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
