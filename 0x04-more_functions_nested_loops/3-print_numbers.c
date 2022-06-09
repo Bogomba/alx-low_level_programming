66365608a99e08345e46374266a1a06b81d75a81
Bogomba <bridgetbogomba@gmail.com>
2022-06-09 08:50:52 -0700

    spacing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
  * print_numbers - prints the numbers, from 0 to 9
  * @n - chanracter representing integers
  * Description: function that prints the numbers, from 0 to 9
- *Return: printed numbers from 1 to 0
+ *Return: void
  */
 void print_numbers(void)
 {
