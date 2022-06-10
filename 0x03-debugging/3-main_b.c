edfa55c1598dad203ce12db5ff7f213e128269b2
Bogomba <bridgetbogomba@gmail.com>
2022-06-09 21:34:36 -0700

    infitite loop

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,45 +1,30 @@
-#include <stdio.h>
-
-#include "main.h"
-
-
-
+#include"main.h"
 /**
- *
- * * main - prints the largest of 3 integers
- *
- * * Return: 0
- *
- * */
-
-
-
-int main(void)
-
+ * _remaining_days -  prints days left to year end
+ * Return: Void
+ * Desription: Printsno of days left to end of the year
+ */
+int print_remaining_days(int)
 {
-
-	        int a, b, c;
-
-		        int largest;
-
-
-
-			        a = 972;
-
-				        b = -98;
-
-					        c = 0;
-
-
-
-						        largest = largest_number(a, b, c);
-
-
-
-							        printf("%d is the largest number\n", largest);
-
-
-
-								        return (0);
-
-}
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 311 
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
+	}                                                                                                                                                                                   
+	
+	}
