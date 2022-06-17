74d0b129c4b7aaad7bd72b22e5ee52eae352b335
Bogomba <bridgetbogomba@gmail.com>
2022-06-17 01:22:00 -0700

    amend

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,9 +16,11 @@ char *_strcat(char *dest, char *src)
 	int i = strlen(dest);
 	int j = strlen(src);
 
-	for (j = 0; src[j]; i++, j++)
-	dest[i + 1] = src[j + 2];
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+	dest[i + 1] = src[j];
 	printf("%s%s", dest, src);
 	printf("%s", src);
+	}
 	return (0);
 }
